4e1c674ea301ec05433c8d2785e6bf39c7e5b2a6
VSJMilewski <victormilewski@gmail.com>
2026-02-05 22:48:57 +0100

    fix duplicate code and variable naming mistake

--- lineup_info_collector/crawlers/lineup_crawler.py
+++ lineup_info_collector/crawlers/lineup_crawler.py
@@ -132,7 +132,8 @@ def _lowlands_crawler2(params):
 def _str_dayfinder(txt, day):
     if txt.find(day) >= 0:
         return day
-    else: return 0
+    else:
+        return 0
 
 def _str_weekend_dayfinder(txt):
     if _str_dayfinder(txt, "Friday"): return "Friday"
@@ -143,21 +144,14 @@ def _bks_crawler(params):
     soup = _get_soup(params["URL"])
     artists = []
     for div in soup.findAll("a", {"class": "act"}):
-        artists.append({"name": div.figure.div.h3.text.strip(), "link": "https://www.bestkeptsecret.nl"+div.attrs["href"], "day": _str_weekend_dayfinder(div.text.strip()), "backup_styles": div.figure.div.span.text.strip()})
-    return artists
-
-
-def _str_weekend_dayfinder(txt):
-    if _str_dayfinder(txt, "Friday"): return "Friday"
-    elif _str_dayfinder(txt, "Saturday"): return "Saturday"
-    elif _str_dayfinder(txt, "Sunday"): return "Sunday"
-
-def _bks_crawler(params):
-    soup = _get_soup(params["URL"])
-    artists = []
-    for div in soup.findAll("a", {"class": "act"}):
-        artists.append({"name": div.figure.div.h3.text.strip(), "link": "https://www.bestkeptsecret.nl"+div.attrs["href"], "day": _str_weekend_dayfinder(div.text.strip()), "backup_styles": div.figure.div.span.text.strip()})
-    return artists
+        artists.append(
+            {
+                "name": div.figure.div.h3.text.strip(),
+                "link": "https://www.bestkeptsecret.nl" + div.attrs["href"],
+                "day": _str_weekend_dayfinder(div.text.strip()),
+                "backup_styles": div.figure.div.span.text.strip(),
+            }
+        )
 
 def lineup_crawler(params):
     if params["FESTIVAL"] == "DTRH":
@@ -170,9 +164,9 @@ def lineup_crawler(params):
         return _ooto_crawler(params)
     elif params["FESTIVAL"] == "prettypissed":
         return _prettypissed_crawler(params)
-    elif festival_name == "BKS":
+    elif params["FESTIVAL"] == "BKS":
         return _bks_crawler(params)
     raise ValueError(
-        f"Unknown festival {festival_name}. "
-        "Currently accepted are: ['DTRH', 'lowlands', 'pinkpop', 'ooto', 'prettypissed']"
+        f"Unknown festival {params['FESTIVAL']}. "
+        "Currently accepted are: ['DTRH', 'lowlands', 'pinkpop', 'ooto', 'prettypissed', 'BKS']"
     )
